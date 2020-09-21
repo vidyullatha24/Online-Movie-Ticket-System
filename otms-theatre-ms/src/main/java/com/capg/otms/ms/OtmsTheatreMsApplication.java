5494e6e38e05e8f20121fe922be00ed91f4cd4e8
Vidyullatha <acevidyullatha1998@gmail.com>
2020-09-21 19:55:57 +0530

    commit

--- otms-theatre-ms/src/main/java/com/capg/otms/ms/OtmsTheatreMsApplication.java
+++ otms-theatre-ms/src/main/java/com/capg/otms/ms/OtmsTheatreMsApplication.java
@@ -1,4 +1,14 @@
 package com.capg.otms.ms;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
 
 public class OtmsTheatreMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtmsTheatreMsApplication.class, args);
+	}
+
 }
